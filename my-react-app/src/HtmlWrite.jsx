88cffca7997c338974aaeaa0880ef551885296f3
shashank <shashankchandanagiri@gmail.com>
2023-11-05 16:48:05 +0530

    creating Own library and jsx of react

--- my-react-app/src/HtmlWrite.jsx
+++ my-react-app/src/HtmlWrite.jsx
@@ -1,7 +1,8 @@
 function Reactret(){
+    const username='Shashank'
     return(
         <>
-            <h3>code is ready</h3>
+            <h3>code is ready made by {username}</h3>
             <p>This is paragraph tag as you know this file can return only on type of tag but by witing it in div you can return any number of tags or We can enen write this in fragments which is denoted by empty angular braket  </p>
             </>
         
